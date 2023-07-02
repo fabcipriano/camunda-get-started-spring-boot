b033db027911a771210f0fe3689697eada4f7d2f
Fabiano C. de Oliveira <fabcipriano@yahoo.com.br>
2023-07-02 10:28:31 -0300

    Change project to use Camunda 7.8.0

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/home/fabiano/opt/jdk8u372-b07/bin/java --version
+
+echo "First arg: $1"
+
+/home/fabiano/opt/jdk8u372-b07/bin/java -jar ./target/loan-approval-spring-boot-0.0.1-SNAPSHOT.jar --server.port=$1
