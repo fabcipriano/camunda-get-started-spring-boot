050035865c13217bd1f97e6eff50407db830bae4
Fabiano C. de Oliveira <fabiano.oliveira@zup.com.br>
2025-02-22 21:07:13 -0300

    configure Postgress database

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 echo "First arg: $1"
 
-/home/fabiano/opt/jdk8u372-b07/bin/java -jar ./target/loan-approval-spring-boot-0.0.1-SNAPSHOT.jar --server.port=$1
+java -jar ./target/loan-approval-spring-boot-0.0.1-SNAPSHOT.jar --server.port=$1
